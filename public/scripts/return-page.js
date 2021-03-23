606786fb05159fc7d7c45eb5793ff23b70b5f7d5
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-03-23 14:05:42 -0300

    added form contact

--- public/scripts/return-page.js
+++ public/scripts/return-page.js
@@ -1,17 +1,35 @@
 const buttonSetup = document.querySelector("#button-setup");
-const secondaryContent = document.querySelector(".secondary-content__info");
-const returnSetup = document.querySelector(".return");
+const buttonContact = document.querySelector("#button-contact");
+const buttonReturn = document.getElementsByClassName("return");
+const contactInfo = document.querySelector(".contact-content__info");
+const hideInfo = document.querySelector(".hide-information");
+const form = document.querySelector(".form");
+const submitForm = document.querySelector(".form button")
+
+buttonContact.addEventListener("click", () => {
+    contactInfo.classList.remove("hide")
+    contactInfo.classList.add("show")
+});
+
+buttonReturn[1].addEventListener("click", () =>{
+    contactInfo.classList.remove("show")
+    contactInfo.classList.add("hide")
+})
 
 buttonSetup.addEventListener("click", showContent)
 
-returnSetup.addEventListener("click", hideContent)
+buttonReturn[0].addEventListener("click", hideContent)
 
 function showContent() {
-    secondaryContent.classList.remove("hide")
-    secondaryContent.classList.add("show")
+    hideInfo.classList.remove("hide")
+    hideInfo.classList.add("show")
 }
 
 function hideContent() {
-    secondaryContent.classList.remove("show")
-    secondaryContent.classList.add("hide")
+    hideInfo.classList.remove("show")
+    hideInfo.classList.add("hide")
 }
+
+submitForm.addEventListener("click", event => {
+    event.preventDefault()
+})
